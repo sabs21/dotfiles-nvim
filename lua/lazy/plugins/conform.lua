ab11d20c4874da7618af24194f8168a7eb33a962
Nick Sabia <nms00010101@outlook.com>
2026-07-27 18:53:43 -0500

    updated conform to fix auto-formatting issues

--- lua/lazy/plugins/conform.lua
+++ lua/lazy/plugins/conform.lua
@@ -2,15 +2,17 @@ return { {
 	"stevearc/conform.nvim",
 	opts = {
 		formatters_by_ft = {
-			["cpp"] = { "clang_format" },
-			["cs"] = { "csharpier" },
-			["js"] = { "prettierd" },
-			["vue"] = { "prettierd" },
+			javascript = { "prettierd" },
+			typescript = { "prettierd" },
+			vue = { "prettierd" },
+			c = { "clang-format" },
+			cpp = { "clang-format" },
+			csharp = { "csharpier" },
 			["_"] = { "trim_whitespace" }
 		},
 		format_on_save = {
 			lsp_format = "fallback",
-			timeout_ms = 500,
+			timeout_ms = 3000,
 		}
 	},
 } }
