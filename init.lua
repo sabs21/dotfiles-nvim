429dc9f7ff25eed78ffd2b9d039b57723ff3ac84
Nick Sabia <nms00010101@outlook.com>
2026-07-27 19:49:57 -0500

    automatically creates an environment file if one doesn't already exist

--- init.lua
+++ init.lua
@@ -1,3 +1,24 @@
+-- Check if the environment file exists.
+-- If not, create it before the require statement is called.
+local env_file = io.open(".\\lua\\environment.lua", "r")
+if env_file~=nil
+then
+	-- Environment file exists.
+	io.close(env_file)
+else
+	-- Copy the environment example file
+	local example_env = io.open(".\\lua\\environment.lua.example", "r")
+	local example_env_contents = example_env:read("*a")
+	example_env:close()
+
+	-- Paste into a new environment file
+	env_file = io.open(".\\lua\\environment.lua", "w")
+	env_file:write(example_env_contents)
+	env_file:close()
+
+	print("Created an environment file inside the lua folder")
+end
+
 require("environment")
 require("autocmd")
 require("detectOS")
